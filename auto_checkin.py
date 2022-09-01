dcb211bc5b3875314aac4ed44c549253954100f8
3858809 <41558665+3858809@users.noreply.github.com>
2022-09-01 09:59:08 +0800

    TG自动签到

--- auto_checkin.py
+++ auto_checkin.py
@@ -13,22 +13,21 @@ for num in range(len(api_id)):
 	client = TelegramClient(session_name[num], api_id[num], api_hash[num])
 	client.start()
 	for (k,v) in robot_map.items():
-		print("Start checkin: ", k)
+		print("开始签到: ", k)
 		client.send_message(k, v) #设置机器人和签到命令
 		time.sleep(3)
 		@client.on(events.NewMessage(chats=k))
 		async def handler(event):
 			# 获取带按钮的消息
-			print("event.message.text: ", event.message.text)
+			print("获取的信息: ", event.message.text)
 			if "已经签到过了" in event.message.text:
 				#await client.disconnect()
-				print(k)
 				if "@EmbyPublicBot" in k:
 					print("厂妹已经签到过了")
 				elif "@qweybgbot" in k:
 					print("卷毛鼠已经签到过了")
 			elif event.message.buttons:
-				print("开始签到",k)
+				print("发现信息中带按钮")
 				if "@EmbyPublicBot" in k:
 					print("厂妹签到 event.message.raw_text: ", event.message.raw_text)
 					# 获取算式 厂妹
@@ -42,7 +41,6 @@ for num in range(len(api_id)):
 							await button.click()
 							break
 					# 结束异步任务
-					await client.disconnect()
 				elif "qweybgbot" in k:
 					print("卷毛鼠签到 event.message.raw_text: ", event.message.raw_text)
 					# 获取算式 卷毛鼠
@@ -67,8 +65,9 @@ for num in range(len(api_id)):
 							await button.click()
 							break
 					# 结束异步任务
-					await client.disconnect()
 					
 		client.send_read_acknowledge(k)	#将机器人回应设为已读
+		print("结束签到: ", k)
+		
 	print("Done! Session name:", session_name[num])	
 os._exit(0)
