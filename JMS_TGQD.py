eaf15fc06af2c0bc5dba971abd4708e8e7925db3
3858809 <41558665+3858809@users.noreply.github.com>
2022-09-01 11:28:37 +0800

    卷毛鼠TG自动签到

--- JMS_TGQD.py
+++ JMS_TGQD.py
@@ -6,7 +6,6 @@ api_id = [5672799]	#输入api_id，一个账号一项
 api_hash = ['e08529171140eac69071c630f03f1a7a']	#输入api_hash，一个账号一项
 
 robot_map = {'@qweybgbot':'/checkin'}
-qdzt = 0
 n = 1
 session_name = api_id[:]
 print("开始签到")
@@ -16,7 +15,7 @@ for num in range(len(api_id)):
 	client = TelegramClient(session_name[num], api_id[num], api_hash[num])
 	client.start()
 	
-	while qdzt == 0:
+	while n < 3:
 		print("当前签到次数:", n)
 		n += 1
 		for (k,v) in robot_map.items():
@@ -28,10 +27,8 @@ for num in range(len(api_id)):
 				# 获取带按钮的消息
 				print("获取的信息: ", event.message.text)
 				if "您距离下次可签到时间还剩" in event.message.text:
-					qdzt = 1
+					n += 10
 					print("已经签到过了")
-					client.send_read_acknowledge(k)	#将机器人回应设为已读
-					break
 				elif event.message.buttons:
 					# 获取算式 卷毛鼠
 					# '请回答下面的问题：\n32 處以 4 = ? (请在60秒内回答)'
@@ -46,18 +43,18 @@ for num in range(len(api_id)):
 					result = int(formula.split(' ')[0]) + int(formula.split(' ')[2])
 					if "加" == js or "+" == js or "加以" == js or "枷" == js:
 						result = int(formula.split(' ')[0]) + int(formula.split(' ')[2])
-						qdzt = 1
+						n += 10
 					elif "减" == js or "-" == js or "缄" == js:
 						result = int(formula.split(' ')[0]) - int(formula.split(' ')[2])
-						qdzt = 1
+						n += 10
 					elif "乘" == js or "*" == js or "乗以" == js or "騬以" == js or "×" == js:
 						result = int(formula.split(' ')[0]) * int(formula.split(' ')[2])
-						qdzt = 1
+						n += 10
 					elif "除" == js or "/" == js or "除以" == js or "處以" == js:
 						result = int(formula.split(' ')[0]) / int(formula.split(' ')[2])
-						qdzt = 1
+						n += 10
 					else:
-						qdzt = 0
+						n += 1
 					print("计算结果:", result)
 					# 匹配按钮文本并点击
 					for button in event.message.buttons[0]:
@@ -65,9 +62,9 @@ for num in range(len(api_id)):
 							button.click()
 							break
 				else:	
-					qdzt = 0
+					n += 1
 					time.sleep(5)
-					
-		print("Done! Session name:", session_name[num])	
-		print("qdzt:", qdzt)	
+			client.send_read_acknowledge(k)	#将机器人回应设为已读
+			
+		print("Done! Session name:", session_name[num])		
 os._exit(0)
