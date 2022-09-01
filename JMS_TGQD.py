842532897b7e45cb626f36273f36cac8938c7f1c
3858809 <41558665+3858809@users.noreply.github.com>
2022-09-01 11:40:24 +0800

    Update JMS_TGQD.py

--- JMS_TGQD.py
+++ JMS_TGQD.py
@@ -6,7 +6,6 @@ api_id = [5672799]	#输入api_id，一个账号一项
 api_hash = ['e08529171140eac69071c630f03f1a7a']	#输入api_hash，一个账号一项
 
 robot_map = {'@qweybgbot':'/checkin'}
-n = 1
 session_name = api_id[:]
 print("开始签到")
 
@@ -15,57 +14,45 @@ for num in range(len(api_id)):
 	client = TelegramClient(session_name[num], api_id[num], api_hash[num])
 	client.start()
 	
-	while n < 3:
-		print("当前签到次数:", n)
-		n += 1
-		for (k,v) in robot_map.items():
-			client.send_message(k, v) #设置机器人和签到命令
-			time.sleep(3)
-			@client.on(events.NewMessage(chats=k))
-			async def handler(event):
-				print("当前签到机器人:", k)
-				# 获取带按钮的消息
-				print("获取的信息: ", event.message.text)
-				if "您距离下次可签到时间还剩" in event.message.text:
-					n += 10
-					print("已经签到过了")
-					await client.disconnect()
-				elif event.message.buttons:
-					# 获取算式 卷毛鼠
-					# '请回答下面的问题：\n32 處以 4 = ? (请在60秒内回答)'
-					formula = ""
-					if k == "@qweybgbot":
-						formula = event.message.raw_text.split('\n')[1]  #处理卷毛鼠签到格式
-					elif k == "@EmbyPublicBot":
-						formula = event.message.raw_text.split('\n\n')[1]  #处理终点站签到格式
-					# 计算结果 25 + 1 = ?
-					js = formula.split(' ')[1]
-					print("计算符号:", js)
+	for (k,v) in robot_map.items():
+		client.send_message(k, v) #设置机器人和签到命令
+		time.sleep(3)
+		@client.on(events.NewMessage(chats=k))
+		async def handler(event):
+			print("当前签到机器人:", k)
+			# 获取带按钮的消息
+			print("获取的信息: ", event.message.text)
+			if "您距离下次可签到时间还剩" in event.message.text:
+				print("已经签到过了")
+				
+			elif event.message.buttons:
+				# 获取算式 卷毛鼠
+				# '请回答下面的问题：\n32 處以 4 = ? (请在60秒内回答)'
+				formula = ""
+				if k == "@qweybgbot":
+					formula = event.message.raw_text.split('\n')[1]  #处理卷毛鼠签到格式
+				elif k == "@EmbyPublicBot":
+					formula = event.message.raw_text.split('\n\n')[1]  #处理终点站签到格式
+				# 计算结果 25 + 1 = ?
+				js = formula.split(' ')[1]
+				print("计算符号:", js)
+				result = int(formula.split(' ')[0]) + int(formula.split(' ')[2])
+				if "加" == js or "+" == js or "加以" == js or "枷" == js:
 					result = int(formula.split(' ')[0]) + int(formula.split(' ')[2])
-					if "加" == js or "+" == js or "加以" == js or "枷" == js:
-						result = int(formula.split(' ')[0]) + int(formula.split(' ')[2])
-						n += 10
-					elif "减" == js or "-" == js or "缄" == js:
-						result = int(formula.split(' ')[0]) - int(formula.split(' ')[2])
-						n += 10
-					elif "乘" == js or "*" == js or "乗以" == js or "騬以" == js or "×" == js:
-						result = int(formula.split(' ')[0]) * int(formula.split(' ')[2])
-						n += 10
-					elif "除" == js or "/" == js or "除以" == js or "處以" == js:
-						result = int(formula.split(' ')[0]) / int(formula.split(' ')[2])
-						n += 10
-					else:
-						n += 1
-					print("计算结果:", result)
-					# 匹配按钮文本并点击
-					for button in event.message.buttons[0]:
-						if int(button.text) == result:
-							await button.click()
-							break
-				else:	
-					n += 1
-					time.sleep(5)
-			client.send_read_acknowledge(k)	#将机器人回应设为已读
-			
-		print("Done! Session name:", session_name[num])		
+				elif "减" == js or "-" == js or "缄" == js:
+					result = int(formula.split(' ')[0]) - int(formula.split(' ')[2])
+				elif "乘" == js or "*" == js or "乗以" == js or "騬以" == js or "×" == js:
+					result = int(formula.split(' ')[0]) * int(formula.split(' ')[2])
+				elif "除" == js or "/" == js or "除以" == js or "處以" == js:
+					result = int(formula.split(' ')[0]) / int(formula.split(' ')[2])
+				print("计算结果:", result)
+				# 匹配按钮文本并点击
+				for button in event.message.buttons[0]:
+					if int(button.text) == result:
+						await button.click()
+						break
+
+		client.send_read_acknowledge(k)	#将机器人回应设为已读
+		client.disconnect()
+	print("Done! Session name:", session_name[num])		
 os._exit(0)
