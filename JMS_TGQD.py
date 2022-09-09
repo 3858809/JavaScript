3fd57df0b441700b1667e38ed5599cf16eb503f1
3858809 <41558665+3858809@users.noreply.github.com>
2022-09-09 09:07:57 +0800

    Update JMS_TGQD.py

--- JMS_TGQD.py
+++ JMS_TGQD.py
@@ -2,6 +2,8 @@ import os
 import time
 from telethon import TelegramClient, events, sync
 
+global XH
+
 api_id = [5672799]	#输入api_id，一个账号一项
 api_hash = ['e08529171140eac69071c630f03f1a7a']	#输入api_hash，一个账号一项
 
@@ -33,9 +35,9 @@ for num in range(len(api_id)):
 	client.start()
 	
 	for (k,v) in robot_map.items():
-		i = 0
-		while i<10:
-			i += 1
+		XH = 0
+		while XH<10:
+			XH += 1
 			client.send_message(k, v) #设置机器人和签到命令
 			time.sleep(3)
 			@client.on(events.NewMessage(chats=k))
@@ -45,7 +47,7 @@ for num in range(len(api_id)):
 				print("获取的信息: ", event.message.text)
 				if "您距离下次可签到时间还剩" in event.message.text or "已经签到过了" in event.message.text:
 					print("已经签到过了")
-					i += 100
+					XH += 100
 				elif event.message.buttons:
 					# 获取算式 卷毛鼠
 					# '请回答下面的问题：\n32 處以 4 = ? (请在60秒内回答)'
@@ -60,7 +62,7 @@ for num in range(len(api_id)):
 					result = fujs(js,formula)
 					print("计算结果:", result)
 					if result != 10086 :
-						i += 100
+						XH += 100
 						
 					# 匹配按钮文本并点击
 					for button in event.message.buttons[0]:
