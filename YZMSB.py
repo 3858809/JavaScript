5c3410b417cd1732b13052996b805a22ab193eaa
3858809 <41558665+3858809@users.noreply.github.com>
2022-09-17 08:28:12 +0800

    Update YZMSB.py

--- YZMSB.py
+++ YZMSB.py
@@ -4,21 +4,19 @@ import json
 import os
 import re
 import requests
-from telethon import TelegramClient, events, sync
-
-def yzm(mc):
-	print("开始识别验证码图片")
-  # 使用 TRUECAPTCHA 模块解析验证码
-  solved_result = captcha_solver("captcha.jpg")
-  if not "result" in solved_result:
-    await client.send_message(CHANNEL_ID, "21342")
-    return
-  captcha_code = handle_captcha_solved_result(solved_result)
-  await client.send_message(event.message.chat_id, captcha_code)
-  # 删除临时文件
-	return jg
 
 
-result = yzm('captcha.jpg')
+def solve(f):
 
-os._exit(0)
+with open(f, "rb") as image_file:
+	encoded_string = base64.b64encode(image_file.read()).decode('ascii')
+	url = 'https://api.apitruecaptcha.org/one/gettext'
+
+	data = { 
+		'userid':'sheriqiang@gmail.com', 
+		'apikey':'L7GYXVaB2BreQrGhzh3I',  
+		'data':encoded_string
+	}
+	response = requests.post(url = url, json = data)
+	data = response.json()
+	return data
