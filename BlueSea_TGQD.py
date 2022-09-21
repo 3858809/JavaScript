341795e9adc15e1be2210df7fdaee526368a104d
3858809 <41558665+3858809@users.noreply.github.com>
2022-09-21 15:29:32 +0800

    Update BlueSea_TGQD.py

--- BlueSea_TGQD.py
+++ BlueSea_TGQD.py
@@ -1,48 +1,58 @@
+import os
+import time
 import asyncio
 import base64
 import json
-import os
 import re
 import requests
-import time
 
-from telethon import TelegramClient, events
+from telethon import TelegramClient, events, sync
 
-API_ID = 5672799	#输入api_id，一个账号一项
-API_HASH = 'e08529171140eac69071c630f03f1a7a'	#输入api_hash，一个账号一项
+api_id = 5672799	#输入api_id，一个账号一项
+api_hash = 'e08529171140eac69071c630f03f1a7a'	#输入api_hash，一个账号一项
+
+i = 0
+
+def captcha_solver(f):
+	with open(f, "rb") as image_file:
+		encoded_string = base64.b64encode(image_file.read()).decode('ascii')
+		url = 'https://api.apitruecaptcha.org/one/gettext'
+		data = { 
+			'userid':'sheriqiang@gmail.com', 
+			'apikey':'L7GYXVaB2BreQrGhzh3I',  
+			'data':encoded_string
+		}
+		response = requests.post(url = url, json = data)
+		data = response.json()
+		return data['result']
 
 async def main():
 	MSG = '/checkin'
 	CHANNEL_ID = '@blueseamusic_bot'
-	print("开始签到!签到对象:",CHANNEL_ID)
-	async with TelegramClient("CLIENT_NAME", API_ID, API_HASH) as client:
+	async with TelegramClient("CLIENT_NAME", api_id, api_hash) as client:
 		await client.send_message(CHANNEL_ID, MSG)
 		time.sleep(3)
 		@client.on(events.NewMessage(chats=CHANNEL_ID))
 		async def handler(event):
-			# 根据button count 区分消息类型
-			print("获取的信息:",event.message.text)
-			if "签到成功" in event.message.text or "上次签到" in event.message.text:
-				print("签到成功")
-				# 结束异步任务
-				await client.disconnect()
+			global i
+			print("获取的信息: ", event.message.text)
+			if "您距离下次可签到时间还剩" in event.message.text or "已经签到过了" in event.message.text:
+				print("已经签到过了")
+				i = 1
 			elif "请输入验证码" in event.message.text:  # 获取图像验证码
 				print("开始处理验证码签到!")
 				print("开始下载")
-				path = await events.message.download_media()
-				print("下载完毕!",path)
+				await client.download_media(event.message.photo, "captcha.jpg")
+				time.sleep(5)
+				print("下载完毕!")
 				# 使用 TRUECAPTCHA 模块解析验证码
-				#solved_result = captcha_solver("captcha.jpg")
-				#if not "result" in solved_result:
-				#	await client.send_message(CHANNEL_ID, "21342")
-				#	return
-				#captcha_code = handle_captcha_solved_result(solved_result)
-				#await client.send_message(event.message.chat_id, captcha_code)
+				solved_result = captcha_solver("captcha.jpg")
+				print("solved_result=",solved_result)
+				await client.send_message(event.message.chat_id, solved_result)
 				# 删除临时文件
-				#os.remove("captcha.jpg")
-			elif "已签过到" in event.message.text:  # 已经签到过
-				print("已签过到")
-			# 是否成功签到
-			elif "验证码错误" in event.message.text:
-				await client.send_message(event.message.chat_id, RETURE_MENU)
-asyncio.run(main())
+				os.remove("captcha.jpg")
+		await client.send_read_acknowledge(CHANNEL_ID)	#将机器人回应设为已读
+		await client.disconnect()
+
+while i == 0:
+	asyncio.run(main())
