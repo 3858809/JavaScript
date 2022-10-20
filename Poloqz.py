e05c9e522fba866a4cba0d367fed966b5171269f
3858809 <41558665+3858809@users.noreply.github.com>
2022-10-20 08:40:31 +0800

    Polo抢注

---
+++ Poloqz.py
@@ -0,0 +1,32 @@
+import os
+import time
+import asyncio
+import base64
+import json
+import re
+import requests
+import datetime
+
+from telethon import TelegramClient,sync
+from telethon.tl.types import InputMessagesFilterPhotos
+proxy = None
+# =============需要被替换的值=================
+'''
+api_id 你的api id
+api_hash 你的api hash
+channel_link 要下载图片的频道链接
+proxy 将localhost改成代理地址,12345改成代理端口
+picture_storage_path 图片下载到的路径
+'''
+api_id = 5672799
+api_hash = "e08529171140eac69071c630f03f1a7a"
+channel_link = "@polo_emby_bot"
+QDmeg = "/create"
+#proxy =("socks5","localhost",12345) #不需要代理的话删掉该行
+# ==========================================
+client = TelegramClient('shexiaoyu',api_id=api_id,api_hash=api_hash,proxy=proxy).start()
+
+client.send_message(channel_link, QDmeg) #发送签到验证码
+client.send_read_acknowledge(channel_link) #将机器人回应设为已读
+client.disconnect()
+print("脚本结束")
