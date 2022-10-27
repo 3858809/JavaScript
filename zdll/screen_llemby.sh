3def37ab80b997f85417eb8c5d8ddc15e52c2e4b
3858809 <41558665+3858809@users.noreply.github.com>
2022-10-28 07:50:09 +0800

    Add files via upload

---
+++ zdll/screen_llemby.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#变量screen名称
+screen_name1="zdll"
+#变量命令
+#cmd1="browsh"
+pkill firefox 
+pkill browsh
+#screen -S zdll -X quit
+#创建screen但不进入
+echo -e `date '+%Y-%m-%d %H:%M:%S %A'` "\n执行脚本开始" >> /home/screen_llemby.txt
+killall screen
+screen -dmS $screen_name1
+#执行指令、输入回车分割。
+screen -x -S $screen_name1 -p 0 -X stuff "browsh https://pornemby.club/web/index.html" 
+screen -x -S $screen_name1 -p 0 -X stuff '\n' 
+sleep 10 
+#screen -x -S $screen_name1 -p 0 -X stuff '\003' #\003代表  ctrl+c 
+#screen -S zdll -X quit
+killall screen
+echo -e `date '+%Y-%m-%d %H:%M:%S %A'` "\n执行脚本结束" >> /home/screen_llemby.txt
