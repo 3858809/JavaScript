f4ed08aa20eaeef91779d8e7035811c352a440b4
3858809 <41558665+3858809@users.noreply.github.com>
2023-01-31 14:40:13 +0800

    甲骨文自动占用25%CPU脚本

---
+++ cpu_25percent.sh
@@ -0,0 +1,30 @@
+cpuq=$(lscpu | awk '/^CPU\(/{print $NF*100/4}');
+[ -d "/opt/shuaibi" ] || mkdir -p /opt/shuaibi;
+cat << eof > /opt/shuaibi/cpu.sh;
+cpuc=$(lscpu | awk '/^CPU\(/{print $NF}');
+for ((i=0;i<cpuc;i++))
+do
+    {
+        dd if=/dev/zero of=/dev/null
+    } &
+done
+wait
+eof
+
+cat << eof > /lib/systemd/system/cpur.service
+[Unit]
+Description=cpu stress 25 percents
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /opt/shuaibi/cpu.sh
+CPUQuota=${cpuq}%
+
+[Install]
+WantedBy=multi-user.target
+eof
+
+systemctl daemon-reload;
+systemctl enable cpur --now;
+systemctl restart cpur;
